c01d75a1dffcc66c91f5a81faa8e12249617b80f
Brandon Higashi <bhigashi169@gmail.com>
2013-10-01 19:05:15 -1000

    October 1, 2013

--- gen/bb/sxytm/freedum/R.java
+++ gen/bb/sxytm/freedum/R.java
@@ -24,13 +24,29 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int RelativeLayout1=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int button_login=0x7f080005;
+        public static final int forgot_password=0x7f080004;
+        public static final int passwordLogin=0x7f080002;
+        public static final int signUp=0x7f080003;
+        public static final int usernameLogin=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_list=0x7f030000;
+        public static final int activity_login=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_month=0x7f030003;
+        public static final int activity_new_event=0x7f030004;
+        public static final int activity_sign_up=0x7f030005;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int list=0x7f070000;
+        public static final int login=0x7f070001;
+        public static final int main=0x7f070002;
+        public static final int month=0x7f070003;
+        public static final int new_event=0x7f070004;
+        public static final int sign_up=0x7f070005;
     }
     public static final class string {
         public static final int Day=0x7f050003;
@@ -40,6 +56,16 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int login_button=0x7f05000f;
+        public static final int password_forgot=0x7f05000e;
+        public static final int password_hint=0x7f05000d;
+        public static final int signup_button=0x7f050010;
+        public static final int title_activity_list=0x7f05000b;
+        public static final int title_activity_login=0x7f050008;
+        public static final int title_activity_month=0x7f05000a;
+        public static final int title_activity_new_event=0x7f050009;
+        public static final int title_activity_sign_up=0x7f050007;
+        public static final int username_hint=0x7f05000c;
     }
     public static final class style {
         /** 
