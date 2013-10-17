21bdc0f165c221debfe5aa7670f940db1e369ed7
Brandon Higashi <bhigashi169@gmail.com>
2013-10-17 13:57:54 -1000

    October 17, 2013

--- gen/bb/sxytm/freedum/R.java
+++ gen/bb/sxytm/freedum/R.java
@@ -21,7 +21,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int eleven_degree=0x7f020000;
+        public static final int fifty_six_degree=0x7f020001;
+        public static final int fourty_five_degree=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int seventy_eight_degree=0x7f020004;
+        public static final int sixty_seven_degree=0x7f020005;
+        public static final int thirty_three_degree=0x7f020006;
+        public static final int zero_degree=0x7f020007;
     }
     public static final class id {
         public static final int Logout=0x7f08001a;
