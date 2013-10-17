1c258ef94d5b30d3662f903038999f176eb89085
Brandon Higashi <bhigashi169@gmail.com>
2013-10-16 23:03:45 -1000

    October 16, 2013

--- src/bb/sxytm/freedum/NewEventActivity.java
+++ src/bb/sxytm/freedum/NewEventActivity.java
@@ -2,7 +2,9 @@ package bb.sxytm.freedum;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.view.Menu;
+import android.view.View;
 
 public class NewEventActivity extends Activity {
 
@@ -18,5 +20,18 @@ public class NewEventActivity extends Activity {
 		getMenuInflater().inflate(R.menu.new_event, menu);
 		return true;
 	}
+	
+	public void showTimePicker(View v) {
+		DialogFragment newFragment = new TimePickerFragment();
+		newFragment.show(getFragmentManager(), "timePicker");
+	}
+	
+	public void showDatePicker(View v) {
+		DialogFragment newFragment = new DatePickerFragment();
+		newFragment.show(getFragmentManager(), "datePicker");
+	}
 
+	public void cancel(View v) {
+		finish();
+	}
 }
